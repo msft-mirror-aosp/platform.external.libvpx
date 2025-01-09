eecfe0afa72a497fa1b974729d96838734d47bb3
James Zern <jzern@google.com>
2025-01-08 19:04:24 -0800

    post_update.sh: set -e outside of shebang
    This avoids differences in behavior if the script is invoked with
    another shell, e.g., bash ./post_update.sh.
    
    Bug: 388605258
    Test: TreeHugger
    Change-Id: Id2cc91ca0149589ebd30ee88529f13204ca61857

--- post_update.sh
+++ post_update.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 #
 # Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
@@ -16,6 +16,8 @@
 # $ external/libvpx/post_update.sh
 #
 # And this will update all the config files needed.
+
+set -e
 
 export LC_ALL=C
 cd $(dirname $0)
